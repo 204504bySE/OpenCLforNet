9b185282134c67b228f03fd279b19db5ebc26aed
204504bySE <204504byse@204504byse.info>
2021-10-17 01:56:57 +0900

    Dispose mem.***Unsafe

--- OpenCLforNetTest/MappingMemoryTest.cs
+++ OpenCLforNetTest/MappingMemoryTest.cs
@@ -26,15 +26,15 @@ namespace OpenCLforNetTest
                 fixed (void* datPtr = dat)
                 fixed (void* ansPtr = ans)
                 {
-                    mem.WriteUnsafe(CommandQueue, true, 0, dat.Length, datPtr).Wait();
+                    mem.WriteUnsafe(CommandQueue, true, 0, dat.Length, datPtr).Dispose();
 
-                    mem.ReadUnsafe(CommandQueue, true, 0, ans.Length, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, 0, ans.Length, ansPtr).Dispose();
                     Assert.AreEqual(dat, ans);
 
-                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Dispose();
                     Assert.AreEqual(dat, ans);
 
-                    mem.WriteUnsafe(CommandQueue, true, datPtr).Wait();
+                    mem.WriteUnsafe(CommandQueue, true, datPtr).Dispose();
                     Assert.AreEqual(dat, ans);
                 }
             }
@@ -51,18 +51,18 @@ namespace OpenCLforNetTest
                 fixed (void* datPtr = dat)
                 fixed (void* ansPtr = ans)
                 {
-                    mem.WriteUnsafe(CommandQueue, true, 0, dat.Length, datPtr).Wait();
+                    mem.WriteUnsafe(CommandQueue, true, 0, dat.Length, datPtr).Dispose();
 
-                    mem.ReadUnsafe(CommandQueue, true, 0, 25, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, 0, 25, ansPtr).Dispose();
                     Assert.AreEqual(dat.Skip(0).Take(25), ans);
 
-                    mem.ReadUnsafe(CommandQueue, true, 25, 25, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, 25, 25, ansPtr).Dispose();
                     Assert.AreEqual(dat.Skip(25).Take(25), ans);
 
-                    mem.ReadUnsafe(CommandQueue, true, 50, 25, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, 50, 25, ansPtr).Dispose();
                     Assert.AreEqual(dat.Skip(50).Take(25), ans);
 
-                    mem.ReadUnsafe(CommandQueue, true, 75, 25, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, 75, 25, ansPtr).Dispose();
                     Assert.AreEqual(dat.Skip(75).Take(25), ans);
                 }
             }
@@ -80,17 +80,17 @@ namespace OpenCLforNetTest
                 fixed (byte* ansPtr = ans)
                 {
                     // 0-clear
-                    mem.WriteUnsafe(CommandQueue, true, 0, ans.Length, ansPtr).Wait();
+                    mem.WriteUnsafe(CommandQueue, true, 0, ans.Length, ansPtr).Dispose();
 
 
-                    mem.WriteUnsafe(CommandQueue, true, 0, 25, datPtr).Wait();
-                    mem.ReadUnsafe(CommandQueue, true, 0, 25, ansPtr).Wait();
+                    mem.WriteUnsafe(CommandQueue, true, 0, 25, datPtr).Dispose();
+                    mem.ReadUnsafe(CommandQueue, true, 0, 25, ansPtr).Dispose();
                     Assert.AreEqual(dat.Take(25), ans.Take(25));
 
-                    mem.WriteUnsafe(CommandQueue, true, 25, 25, datPtr + 25).Wait();
-                    mem.WriteUnsafe(CommandQueue, true, 50, 25, datPtr + 50).Wait();
-                    mem.WriteUnsafe(CommandQueue, true, 75, 25, datPtr + 75).Wait();
-                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Wait();
+                    mem.WriteUnsafe(CommandQueue, true, 25, 25, datPtr + 25).Dispose();
+                    mem.WriteUnsafe(CommandQueue, true, 50, 25, datPtr + 50).Dispose();
+                    mem.WriteUnsafe(CommandQueue, true, 75, 25, datPtr + 75).Dispose();
+                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Dispose();
                     Assert.AreEqual(dat, ans);
                 }
             }
@@ -114,7 +114,7 @@ namespace OpenCLforNetTest
                 byte[] ans = new byte[100];
                 fixed (void* ansPtr = ans)
                 {
-                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Dispose();
                     Assert.AreEqual(Enumerable.Range(0, 100).Select(n => (byte)n), ans);
                 }
             }
@@ -136,7 +136,7 @@ namespace OpenCLforNetTest
                 byte[] ans = new byte[100];
                 fixed (void* ansPtr = ans)
                 {
-                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Dispose();
                     Assert.AreEqual(expected.Take(50), ans.Take(50));
                 }
 
@@ -147,7 +147,7 @@ namespace OpenCLforNetTest
 
                 fixed (void* ansPtr = ans)
                 {
-                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Wait();
+                    mem.ReadUnsafe(CommandQueue, true, ansPtr).Dispose();
                     Assert.AreEqual(expected, ans);
                 }
             }
